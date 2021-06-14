06688bd87e52d55d9b61044b6a694c9b943caa3f
Eugene753 <Imarkadanov@gmail.com>
2021-06-14 13:30:48 -0400

    Solar system basic structure is ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // developing basic solar system
+	SolarSystem FactorX=new SolarSystem();
+	FactorX.Sun=1;
+	FactorX.Moon=3;
+	FactorX.Planets=2;
     }
 }
